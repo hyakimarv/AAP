ac53a09f7c728d998402fb6de8ff3b7393b7fde6
hyakinarv <vitek.chvojka@infis.cz>
2025-03-18 09:32:00 +0100

    test.3

--- ukol.1.py
+++ ukol.1.py
@@ -1,1 +1,3 @@
-#test
+name=input("Jak se jmenuješ ")
+age=input("Kolik ti je? ")
+print(f"jmenuješ se {name} a je ti {age} let")
--- ukol.2.py
+++ ukol.2.py
@@ -1,1 +1,10 @@
-#test
+hodnota1=float(input("Napiš číslo s desetiným číslem"))
+hodnota2=float(input("Napiš číslo s desetiným číslem"))
+soucet= hodnota1 + hodnota2
+rozdil= hodnota1 - hodnota2
+soucin = hodnota1 * hodnota2
+podil= hodnota1 / hodnota1
+print(f"soucet: {soucet:.2f}")
+print(f"rozdil: {rozdil:.2f}")
+print(f"soucin: {soucin:.2f}")
+print(f"podil: {podil:.2f}")
